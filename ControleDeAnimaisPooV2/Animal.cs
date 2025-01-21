4dc4b2685bf7201de1a7df8dea54dd9dac607b9c
Guilherme <Guilherme_ferreiraDev@outlook.com>
2025-01-21 11:06:42 -0300

    first commit

---
+++ ControleDeAnimaisPooV2/Animal.cs
@@ -0,0 +1,29 @@
+﻿namespace ControleDeAnimaisPooV2
+{
+    internal class Animal
+    {
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private string tipo;
+        public string Tipo
+        {
+            get { return tipo; }
+            set
+            {               
+                if (value == "CACHORRO" || value == "GATO" || value == "PEIXE")
+                {
+                    tipo = value; 
+                }
+                else
+                {
+                    tipo = "PEIXE"; 
+                }
+            }
+        }
+    }
+}
---
+++ ControleDeAnimaisPooV2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ControleDeAnimaisPooV2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int quantidadeCachorro = 0, quantidadeGato = 0, quantidadePeixe = 0;
+
+            Console.WriteLine("Controle de Animais \n");
+
+            do
+            {
+                Animal animal = new Animal();
+
+                Console.WriteLine("Digite o nome do " + contador + "° Animal");
+                animal.Nome = Console.ReadLine();
+
+                Console.WriteLine("Digite o tipo do " + contador + "° Animal");
+                animal.Tipo = Console.ReadLine().ToUpper(); 
+
+                if (animal.Tipo == "CACHORRO") quantidadeCachorro++;
+                else if (animal.Tipo == "GATO") quantidadeGato++;
+                else if (animal.Tipo == "PEIXE") quantidadePeixe++;
+
+                contador++;
+            } while (contador <= 5);
+
+            Console.WriteLine($"\nCachorros: {quantidadeCachorro}, Gatos: {quantidadeGato}, Peixes: {quantidadePeixe}");
+            Console.ReadKey();
+        }
+    }
+}
